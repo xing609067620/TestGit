f1ededde6c69f2ac4512d3775021979a01ff8a29
eclipse_user <eclipse_609067620@qq.com>
2018-08-14 11:27:59 +0800

    ge 2  edit ...

--- src/main/java/com/xtc/ApplicationMainSpring.java
+++ src/main/java/com/xtc/ApplicationMainSpring.java
@@ -4,5 +4,6 @@ public class ApplicationMainSpring {
 	public static void main(String[] args) {
 		System.out.println("嗯嗯嗯嗯");
 		System.out.println("是不是只有我还是我");
+		System.out.println("还在感慨,嗨唉一耶");
 	}
 }
